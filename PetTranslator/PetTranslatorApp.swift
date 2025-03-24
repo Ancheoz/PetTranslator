a7c577072c8faf502a87effab1bdc53f94068c2a
7anche7 <sandys7n@gmail.com>
2025-03-24 23:24:41 +0200

    Initial Commit

---
+++ PetTranslator/PetTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetTranslatorApp.swift
+//  PetTranslator
+//
+//  Created by Oleksandr Prytchyn on 24.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct PetTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
