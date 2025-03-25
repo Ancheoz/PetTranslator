772031eb60c7f97767755a779e9659139dfe4394
Ancheoz <sandys7n@gmail.com>
2025-03-26 01:09:14 +0200

    - replace image assets and improve adaptive design on TranslatorView page

--- PetTranslator/Models/ViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ViewModel.swift
-//  PetTranslator
-//
-//  Created by Oleksandr Prytchyn on 24.03.2025.
-//
-
-import Foundation
---
+++ PetTranslator/Views/ClickView.swift
@@ -0,0 +1,16 @@
+//
+//  ClickView.swift
+//  PetTranslator
+//
+//  Created by Oleksandr Prytchyn on 25.03.2025.
+//
+
+import SwiftUI
+
+struct ClickView: View {
+    var body: some View {
+        VStack {
+            Text("ClickView, just a test")
+        }
+    }
+}
